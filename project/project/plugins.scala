7332a8e26ab5d329c3e1fab2f1f389d9fc58cde0
xuwei-k <6b656e6a69@gmail.com>
2013-09-06 01:42:48 +0900

    update conscript-plugin uri

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Plugin extends Build {
   lazy val root = Project("plugins", file(".")) dependsOn(
-    uri("git://github.com/xuwei-k/conscript-plugin.git#sbt0.13.0")
+    uri("git://github.com/n8han/conscript-plugin.git#ff4599bcf5ca9737")
   )
 }
