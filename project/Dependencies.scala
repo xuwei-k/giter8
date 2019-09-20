6d8b7bb6971d06be95860e0188c684a6f2328f5a
xuwei-k <6b656e6a69@gmail.com>
2019-09-20 03:47:53 +0000

    Update io to 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val scalacheck       = "org.scalacheck" %% "scalacheck" % "1.14.0"
   val scalatest        = "org.scalatest" %% "scalatest" % "3.0.8"
   val scalamock        = "org.scalamock" %% "scalamock" % "4.4.0"
-  val sbtIo            = "org.scala-sbt" %% "io" % "1.3.0-M13"
+  val sbtIo            = "org.scala-sbt" %% "io" % "1.3.1"
   val scala212         = "2.12.8"
   val scala213         = "2.13.0"
   val sbt1             = "1.2.8"
